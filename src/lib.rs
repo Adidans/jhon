8e00cce771d16cdb12180f46f99f2fda598e5bb1
Adidans <adrians.uvis@gmail.com>
2026-05-28 22:42:53 +0300

    Set up skeleton for parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,31 @@
+use chumsky::{input::ValueInput, prelude::*};
 use logos::Logos;
+use std::fmt;
 
-#[derive(Debug, Logos, PartialEq)]
+#[derive(Debug, Clone, Logos, PartialEq)]
 #[logos(skip r"[ \t\n\f]+")] // Skip whitespace
 pub enum Token {
+    Error,
+
     #[token("{")]
     LBrace,
     #[token("}")]
     RBrace,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::LBrace => write!(f, "{{"),
+            Token::RBrace => write!(f, "}}"),
+            Token::Error => write!(f, "<error>"),
+        }
+    }
+}
+
+pub fn parser<'tok, I>() -> impl Parser<'tok, I, (), extra::Err<Rich<'tok, Token>>>
+where
+    I: ValueInput<'tok, Token = Token, Span = SimpleSpan>,
+{
+    end()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
-use jhon::Token;
+use ariadne::{Color, Label, Report, ReportKind, Source};
+use chumsky::{input::Stream, prelude::*};
+use jhon::{Token, parser};
 use logos::Logos;
 
 fn main() {
-    let mut lexer = Token::lexer("{}}");
-    for token in lexer {
-        match token {
-            Ok(t) => println!("{t:?}"),
-            Err(e) => eprintln!("Error lexing"),
+    let src = "{}";
+
+    let token_iter = Token::lexer(src).spanned().map(|(tok, span)| match tok {
+        Ok(tok) => (tok, span.into()),
+        Err(()) => (Token::Error, span.into()),
+    });
+
+    let token_stream =
+        Stream::from_iter(token_iter).map((0..src.len()).into(), |(t, s): (_, _)| (t, s));
+
+    match parser().parse(token_stream).into_result() {
+        Ok(expr) => println!("Parsed: {:?}", expr),
+        Err(errs) => {
+            for err in errs {
+                Report::build(ReportKind::Error, ((), err.span().into_range()))
+                    .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+                    .with_code(3)
+                    .with_message(err.to_string())
+                    .with_label(
+                        Label::new(((), err.span().into_range()))
+                            .with_message(err.reason().to_string())
+                            .with_color(Color::Red),
+                    )
+                    .finish()
+                    .eprint(Source::from(src))
+                    .unwrap();
+            }
         }
     }
 }
